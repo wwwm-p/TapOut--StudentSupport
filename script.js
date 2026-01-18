0aa7f304c35d960db0a04a4ae091fb7f3f12cd17
wwwm-p <miap2k10@gmail.com>
2026-01-17 19:49:22 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,19 @@
-let selectedReason = "";
-let selectedUrgency = "";
-
-function goToPage(pageId) {
-  document.querySelectorAll(".page").forEach(p => p.classList.remove("active"));
-  document.getElementById(pageId).classList.add("active");
-}
-
-function chooseReason(reason) {
-  selectedReason = reason;
-  goToPage("page2");
-}
-
-function chooseUrgency(level) {
-  selectedUrgency = level;
-  goToPage("page3");
-}
-
-function sendEmail(counselorEmail) {
-  const name = studentName.value || "Anonymous";
-  const grade = studentGrade.value || "N/A";
+function sendMessage(reason, urgency, counselor) {
+  const name = document.getElementById("studentName").value || "Anonymous";
+  const grade = document.getElementById("studentGrade").value || "N/A";
 
   const messages = JSON.parse(localStorage.getItem("studentMessages") || "[]");
 
   messages.push({
     name,
     grade,
-    reason: selectedReason,
-    urgency: selectedUrgency,
-    counselor: counselorEmail.split("@")[0],
+    reason,
+    urgency,
+    counselor,
     time: new Date().toLocaleString()
   });
 
   localStorage.setItem("studentMessages", JSON.stringify(messages));
-
   alert("Message sent to counselor dashboard!");
 }
+
