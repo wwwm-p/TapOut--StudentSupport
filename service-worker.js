589a758bc2bbaa9b0b197c11519328129b71d176
wwwm-p <miap2k10@gmail.com>
2026-03-23 13:13:25 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,39 @@
-const CACHE_NAME="tapout-student-v1";
-const ASSETS=[
-  "/","/index.html","/style.css","/page1.png","/page2.png","/page3.png",
-  "/student-manifest.json","/icons/icon-192-student.png","/icons/icon-512-student.png"
+const CACHE_NAME = "tapout-v2";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./page1.png",
+  "./page2.png",
+  "./page3.png",
+  "./icons/icon-192-student.png",
+  "./icons/icon-512-student.png"
 ];
 
-self.addEventListener("install", e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener("activate", e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))));self.clients.claim();});
-self.addEventListener("fetch", e=>{
-  if(e.request.mode==="navigate"){e.respondWith(fetch(e.request).catch(()=>caches.match("/")));} 
-  else{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));}
+// install
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// activate
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// fetch
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request).catch(() => caches.match("./index.html"));
+    })
+  );
 });
