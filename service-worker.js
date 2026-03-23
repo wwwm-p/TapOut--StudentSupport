fec02be9d8dacd4ca293f4eeeec638f2338d32e7
wwwm-p <miap2k10@gmail.com>
2026-03-23 11:21:12 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -11,33 +11,24 @@ const ASSETS = [
   "/icons/icon-512-student.png"
 ];
 
-/* INSTALL */
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-/* ACTIVATE */
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))
-    )
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)
+    ))
   );
   self.clients.claim();
 });
 
-/* FETCH */
 self.addEventListener("fetch", event => {
   if(event.request.mode === "navigate"){
-    event.respondWith(
-      fetch(event.request).catch(() => caches.match("/"))
-    );
+    event.respondWith(fetch(event.request).catch(() => caches.match("/")));
   } else {
-    event.respondWith(
-      caches.match(event.request).then(res => res || fetch(event.request))
-    );
+    event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
   }
 });
