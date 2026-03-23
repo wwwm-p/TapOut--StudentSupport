c0350408e374574dfbabac67196efd8b8bff78d9
wwwm-p <miap2k10@gmail.com>
2026-03-23 09:49:38 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,20 @@
 const CACHE_NAME = "tapout-student-v1";
-
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./page1.png",
-  "./page2.png",
-  "./page3.png",
-  "./student-manifest.json",
-  "./icons/icon-192-student.png",
-  "./icons/icon-512-student.png"
+  "/",
+  "/index.html",
+  "/style.css",
+  "/page1.png",
+  "/page2.png",
+  "/page3.png",
+  "/student-manifest.json",
+  "/icons/icon-192-student.png",
+  "/icons/icon-512-student.png"
 ];
 
 /* INSTALL */
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
@@ -26,21 +23,21 @@ self.addEventListener("install", event => {
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      )
+      Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))
     )
   );
   self.clients.claim();
 });
 
-/* FETCH (offline fallback) */
+/* FETCH */
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(res => {
-      return res || fetch(event.request);
-    })
-  );
+  if(event.request.mode === "navigate"){
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match("/"))
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(res => res || fetch(event.request))
+    );
+  }
 });
